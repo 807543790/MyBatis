ec044c5e5e085dee1d1323998797fa1970b5bd10
zhangbin <807553790@qq.com>
2020-07-09 09:42:45 +0800

    添加mybatis第一个查询案例

---
+++ MyBatis02/src/main/java/com/zhangbin/dao/UserMapper.java
@@ -0,0 +1,14 @@
+package com.zhangbin.dao;
+
+import com.zhangbin.pojo.User;
+
+import java.util.List;
+
+/**
+ * 认认真真敲代码，开开心心每一天
+ *
+ * @Date 2020/7/9-9:21
+ */
+public interface UserMapper {
+    List<User> getUserList();
+}
---
+++ MyBatis02/src/test/java/com/zhangbin/dao/UserDaoTest.java
@@ -0,0 +1,31 @@
+package com.zhangbin.dao;
+
+import com.zhangbin.pojo.User;
+import com.zhangbin.utils.MyBatisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * 认认真真敲代码，开开心心每一天
+ *
+ * @Date 2020/7/9-9:26
+ */
+public class UserDaoTest {
+
+    @Test
+    public void test01(){
+        SqlSession sqlSession = MyBatisUtils.getSqlSession();
+
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+
+        List<User> userList = mapper.getUserList();
+
+        for (User user : userList) {
+            System.out.println(user);
+        }
+        sqlSession.close();
+
+    }
+}
